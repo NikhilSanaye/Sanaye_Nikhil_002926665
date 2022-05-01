af3d67a3e6d968974a9314059d01943bd9dc1222
Nikhil Sanaye <sanaye.ni@northeastern.edu>
2022-05-01 19:41:11 -0400

    fix

--- final_project/src/ui/SalesManager/SalesManagerWorkAreaJPanel.java
+++ final_project/src/ui/SalesManager/SalesManagerWorkAreaJPanel.java
@@ -3,9 +3,12 @@ package ui.SalesManager;
 import ui.AdminRole.*;
 import model.SupplierDirectory;
 import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import model.User;
-
+import ui.CustomerRole.CustomerWorkAreaJPanel;
 /**
  *
  * @author  Mihir Mehta / Hechen Gao
@@ -35,6 +38,8 @@ public class SalesManagerWorkAreaJPanel extends javax.swing.JPanel {
         btnManageSuppliers = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         btnManageSuppliers1 = new javax.swing.JButton();
+        btnManageSuppliers2 = new javax.swing.JButton();
+        btnManageSuppliers3 = new javax.swing.JButton();
 
         setPreferredSize(new java.awt.Dimension(650, 600));
 
@@ -56,17 +61,33 @@ public class SalesManagerWorkAreaJPanel extends javax.swing.JPanel {
             }
         });
 
+        btnManageSuppliers2.setText("View Income chart >>");
+        btnManageSuppliers2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageSuppliers2ActionPerformed(evt);
+            }
+        });
+
+        btnManageSuppliers3.setText("View Monthly chart >>");
+        btnManageSuppliers3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageSuppliers3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 626, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
                 .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnManageSuppliers3)
+                    .addComponent(btnManageSuppliers2)
                     .addComponent(btnManageSuppliers1)
                     .addComponent(btnManageSuppliers))
                 .addGap(59, 59, 59))
@@ -80,7 +101,11 @@ public class SalesManagerWorkAreaJPanel extends javax.swing.JPanel {
                 .addComponent(btnManageSuppliers)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnManageSuppliers1)
-                .addGap(429, 429, 429))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnManageSuppliers2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnManageSuppliers3)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -93,12 +118,34 @@ public class SalesManagerWorkAreaJPanel extends javax.swing.JPanel {
 
     private void btnManageSuppliers1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageSuppliers1ActionPerformed
         // TODO add your handling code here:
+        PolarChart test = new PolarChart();
+        test.runChart();
+        
+        
+ 
+    //userProcessContainer.add("TestJpanel", test);
+    //CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+    //layout.next(userProcessContainer);
     }//GEN-LAST:event_btnManageSuppliers1ActionPerformed
+
+    private void btnManageSuppliers2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageSuppliers2ActionPerformed
+        // TODO add your handling code here:showPieChart
+        Main p = new Main();
+        p.showChart();
+    }//GEN-LAST:event_btnManageSuppliers2ActionPerformed
+
+    private void btnManageSuppliers3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageSuppliers3ActionPerformed
+        // TODO add your handling code here:
+        BarChart m = new BarChart();
+        m.ShowBarChart();
+    }//GEN-LAST:event_btnManageSuppliers3ActionPerformed
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnManageSuppliers;
     private javax.swing.JButton btnManageSuppliers1;
+    private javax.swing.JButton btnManageSuppliers2;
+    private javax.swing.JButton btnManageSuppliers3;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
     
