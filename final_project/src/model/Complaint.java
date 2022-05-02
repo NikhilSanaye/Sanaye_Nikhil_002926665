f1a8c4a49e0c56f53dd2822cac81a31dad1fabbc
sanayen <98855730+sanayen@users.noreply.github.com>
2022-05-02 14:10:04 -0400

    updates

---
+++ final_project/src/model/Complaint.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author nsana
+ */
+public class Complaint {
+    
+    private int orderId;
+    private String description;
+    private String productName;
+    private String resolutionType;
+    private String status;
+    
+    private String userName;
+
+    public Complaint() {
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(orderId);
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getResolutionType() {
+        return resolutionType;
+    }
+
+    public void setResolutionType(String resolutionType) {
+        this.resolutionType = resolutionType;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+    
+    
+}
