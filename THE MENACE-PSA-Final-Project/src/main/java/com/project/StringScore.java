6151176a4bda881653f5f2b7ccd2c5154a677a53
Nikhil Sanaye <99310623+NikhilSanaye@users.noreply.github.com>
2022-09-09 00:37:30 -0400

    Add files via upload

---
+++ THE MENACE-PSA-Final-Project/src/main/java/com/project/StringScore.java
@@ -0,0 +1,20 @@
+package com.project;
+
+public class StringScore {
+	//Object to store counter moves & score
+	String counterMove;
+	int score;
+	public String getCounterMove() {
+		return counterMove;
+	}
+	public void setCounterMove(String s) {
+		this.counterMove = s;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
